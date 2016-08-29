09cc10e4d8d6bbc39de299ede54d2437a6ff6c42
garicchi <xgaricchi@gmail.com>
2016-08-29 12:42:13 +0900

    add header

--- category.py
+++ category.py
@@ -16,22 +16,27 @@ def __get_category(app_id,cate_id):
     root = xml.dom.minidom.parseString(xml_data)
     categoriesNode = root.childNodes[0].childNodes[0].childNodes[0].childNodes
     bigCategory = ''
+    bigId = ''
     result = []
     for cate in categoriesNode:
 
         if cate.tagName == 'Current':
+            id = __getElementByTagName(cate,'Id')
+
             title = __getElementByTagName(cate, 'Title')
             short = __getElementByTagName(title, 'Short')
             data = short.childNodes[0].data
             bigCategory = data
+            bigId = id.childNodes[0].data
         if cate.tagName == 'Children':
             childs = __getElementListByTagName(cate,'Child')
             for c in childs:
-
+                id = __getElementByTagName(c, 'Id')
                 title = __getElementByTagName(c, 'Title')
                 short = __getElementByTagName(title, 'Short')
                 data = short.childNodes[0].data
-                result.append((bigCategory,data))
+                id_str = id.childNodes[0].data
+                result.append((bigId,bigCategory,id_str,data))
     return result
 
 
@@ -49,8 +54,9 @@ if __name__ == '__main__':
         all_result.extend(result)
 
     with open('categories.csv','w') as f:
+        f.write('big_id,big_category,small_id,small_category\n')
         for data in all_result:
-            f.write(data[0]+','+data[1]+'\n')
+            f.write(data[0]+','+data[1]+','+data[2]+','+data[3]+'\n')
 
     print('owari')
 
